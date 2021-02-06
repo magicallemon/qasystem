3a11c12dbee8c76ab46276a3e7f781371df125b4
magicallemon <247776195@qq.com>
2021-02-06 23:18:22 +0800

    新建分支，修改mybatis-generator设置，完善

---
+++ src/main/java/com/springboot/qa/qasystem/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.springboot.qa.qasystem.mapper;
+
+import com.springboot.qa.qasystem.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
--- src/test/java/com/springboot/qa/qasystem/QasystemApplicationTests.java
+++ src/test/java/com/springboot/qa/qasystem/QasystemApplicationTests.java
@@ -4,15 +4,16 @@ import com.springboot.qa.qasystem.model.User;
 import com.springboot.qa.qasystem.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class QasystemApplicationTests {
 
-    @Autowired
+    @Resource
     UserService userService;
 
 
